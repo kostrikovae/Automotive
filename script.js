214017739c81406398fea51a41383e6f33dcaebd
kostrikovae <kostrikovae95@mail.ru>
2025-01-20 21:20:19 +0300

    change burger

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    const burgerButton = document.getElementById('burger');
-    const navbarNav = document.getElementById('navbar');
+// document.addEventListener("DOMContentLoaded",()=>{
+//     const burgerButton = document.getElementById('burger');
+//     const navbarNav = document.getElementById('navbar');
 
-    burgerButton.addEventListener("click",()=>{
-      navbarNav.classList.toggle('active')
-    });
+//     burgerButton.addEventListener("click",()=>{
+//       navbarNav.classList.toggle('active')
+//     });
+// });
+
+const burgerButton = document.querySelector('#burger');
+const navbarNav = document.querySelector('.navbar');
+
+burgerButton.addEventListener('click', () => {
+  burgerButton.classList.toggle('actively');
+  navbarNav.classList.toggle('active');
 });
